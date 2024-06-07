b5fb06e51a6b3b64bb7fb4ae84e685632a0a6412
rsetter <rsetter@hawaii.edu>
2024-06-06 18:41:05 -1000

    additional plots

--- 6paperfigs.R
+++ 6paperfigs.R
@@ -1,14 +1,17 @@
+library(ggplot2)
+library(ggpubr)
+library(ggrepel)
 
 #beach cost plot
 #TB total cost y axis
 #small bars = Littoral cell
 #big bars = district
 
-
+source("C:/Users/rsett/Documents/KauaiRetreat/1assessorsk.R")
+source("C:/Users/rsett/Documents/KauaiRetreat/1infrastructurek.R") #takes 15 min to run this code
 allisland <- clean_retreat_calcs
 allinfra <- infra_retreat
 beaches <- unique(allisland$LittrlCell) # 37 distinct 
-districts <- unique(allisland$district) # 6 distinct
 
 scen <- c('AO','TB','RE','AO','TB','RE')
 sw <- c('_','_','_','_','_','_')
@@ -22,6 +25,10 @@ clean <- c(NA,NA,'hi',NA,NA,'hi')
 beachdf <- setNames(data.frame(matrix(ncol = 10, nrow = 0)), c('scenario','trigger','beach','district',
                                                                 'land_dwelling_cost','ambiguous_cost','infrastructure_cost',
                                                                 'tax_revenue_loss','private_property_value_loss','total_cost'))
+#calculate CE/PF per beach over time
+cepfbeach <- setNames(data.frame(matrix(ncol = 4, nrow = 0)), c('beach','year','trigger','area'))
+cepfhazard_types <- c('CE','PF')
+years <- c(2023,2026,2030,2040,2050,2062,2075,2087,2100)
 
 for(beach in beaches){
   clean_retreat_calcs <- allisland[allisland$LittrlCell==beach,]
@@ -57,6 +64,21 @@ for(beach in beaches){
                                       tax_revenue_loss = Retreat_Analysis_Total[[taxrevloss_col]],
                                       private_property_value_loss = Retreat_Analysis_Total[[privproploss_col]],
                                       total_cost = NA)
+    
+  }
+  
+  #calculate erosion/passive flooding over time
+  for(i in 1:length(years)){
+    for(hazard_type in cepfhazard_types){
+      areahazard_col <- paste0("areahazard_l",hazard_type)
+      year <- years[i]
+      prevyear <- years[i-1]
+      
+      totalarea <- ifelse(year==2023,0,as.numeric(cepfbeach$area[cepfbeach$beach == beach & cepfbeach$trigger == hazard_type & cepfbeach$year == prevyear])) +
+        Retreat_Analysis[[areahazard_col]][Retreat_Analysis$Years == year]
+      
+      cepfbeach[nrow(cepfbeach) + 1,] = c(beach = beach,year = year,trigger = hazard_type, area = totalarea)
+    }
   }
 }
 
@@ -72,30 +94,107 @@ beachesdf <- beachdf %>%
 #remove '0' beaches
 beachesdf <- beachesdf[beachesdf$beach != 0,]
 
+# A: total cost
 figbeachA <- ggplot(beachesdf, aes(y=total_cost, x=factor(beach))) + 
   geom_bar(stat='identity',position='dodge')+
   facet_wrap(~factor(scenario,levels=c('AO','TB','RE')), ncol = 3) +
   xlab('beach ID')+
   ylab('total cost')+
   theme_minimal() 
+# B: infrastructure cost
 figbeachB <- ggplot(beachesdf, aes(y=infrastructure_cost, x=factor(beach))) + 
   geom_bar(stat='identity',position='dodge')+
   facet_wrap(~factor(scenario,levels=c('AO','TB','RE')), ncol = 3) +
   xlab('beach ID')+
   ylab('infrastructure cost')+
   theme_minimal() 
+# C: residential cost
 figbeachC <- ggplot(beachesdf, aes(y=residential_cost, x=factor(beach))) + 
   geom_bar(stat='identity',position='dodge')+
   facet_wrap(~factor(scenario,levels=c('AO','TB','RE')), ncol = 3) +
   xlab('beach ID')+
   ylab('residential cost')+
   theme_minimal() 
+#ADD D: Cost per beach length (standardized)
   
 figbeach <- ggarrange(figbeachA,figbeachB,figbeachC,
                   labels=c("A","B","C"),
                   ncol=1,nrow=3,
                   align="v")
 
+
+
+
+
+
+# spike plot
+figspike <- ggplot(beachesdf, aes(y=total_cost, x=factor(beach))) + 
+  geom_bar(stat='identity',position='dodge')+
+  xlab('beach ID')+
+  ylab('total cost')+
+  theme_minimal() +
+  theme(
+    axis.title=element_blank(),
+    axis.text=element_blank(),
+    panel.grid=element_blank()
+  )+
+  coord_polar(start=0)+
+  ylim(-1000000000,max(beachesdf$total_cost)) + #negative space for inner circle
+  facet_wrap(~factor(scenario,levels=c('AO','TB','RE')), ncol = 3) +
+  geom_text(aes(label=beach, vjust=1))
+
+#add coloring by category
+
+
+
+
+
+
+
+# CE/PF over time plot
+beachesdf$combo <- paste0(beachesdf$beach,beachesdf$trigger)
+combos <- unique(beachesdf$combo) 
+cepfbeach$combo <- paste0(cepfbeach$beach,cepfbeach$trigger)
+cepfbeachdf <- cepfbeach %>%
+  filter(
+    combo %in% combos
+  )
+cepfbeachdf$area <- as.numeric(cepfbeachdf$area)
+cepfbeachdf$year <- as.numeric(cepfbeachdf$year)
+beachegce <- c('41','80')
+beachegpf <- c('10')
+cepfbeachdf <- cepfbeachdf %>%
+  rowwise() %>%
+  mutate(label = case_when(beach %in% beachegce && year == 2100 ~ beach,
+                            beach %in% beachegpf && year == 2100 ~ beach))
+
+figcepf <- ggplot(cepfbeachdf, aes(x=year,y=area,group=beach,color=trigger))+
+  geom_line(aes(color=trigger),alpha=0.5,linewidth=1)+
+  scale_color_manual(name="Hazard",labels=c("CE","PF"),
+                     values=c("#FADD21","#2E9FDF"))+ 
+  theme_bw()+
+  scale_y_continuous(name=expression("Total area retreated "~(m^2)),labels = scales::comma)+
+  xlab("Year")
+
+figcepf + geom_line(data=cepfbeachdf[cepfbeachdf$beach %in% beachegce,], color='#E7B800',linewidth=1.3)+
+  geom_line(data=cepfbeachdf[cepfbeachdf$beach %in% beachegpf,], color='#05A1FB',linewidth=1.3)+
+  geom_label_repel(aes(label=label),nudge_x = 1,na.rm=T,show.legend=F)
+
+
+#https://htmlcolorcodes.com/
+
+
+
+
+
+
+
+
+
+
+
+
+#### OLD UNUSED FIGURES
 
 # treemap plot
 # infrastructure vs res color coding
@@ -160,8 +259,8 @@ tm <- tm %>%
 tm$percent <- tm$cost/tm$scenario_sum #sum(tm$cost,na.rm=T)
 
 figpiebeach <- ggplot(tm,aes(area=cost,fill=group,
-                        label=paste(subgroup,scales::percent(percent,accuracy=1),sep ="\n"),
-                        subgroup=subgroup))+
+                             label=paste(subgroup,scales::percent(percent,accuracy=1),sep ="\n"),
+                             subgroup=subgroup))+
   treemapify::geom_treemap(layout="squarified")+
   geom_treemap_text(color='white',place = "centre",size = 15)+
   geom_treemap_subgroup_border(colour = "white", size = 3)+
@@ -173,22 +272,9 @@ figpiebeach <- ggplot(tm,aes(area=cost,fill=group,
   facet_wrap(~factor(scenario,levels=c('AO','TB','RE')), ncol = 3) +
   theme(legend.position="bottom",strip.text.x = element_text(size = 15),panel.spacing=unit(1,"lines"))
 
-
-
-#
-
-
-
-
-
-
-
-
-
-
-
-
 #calculate cost per district
+districts <- unique(allisland$district) # 6 distinct
+
 maheledistdf <- setNames(data.frame(matrix(ncol = 10, nrow = 0)), c('scenario','trigger','beach','district',
                                                                     'land_dwelling_cost','ambiguous_cost','infrastructure_cost',
                                                                     'tax_revenue_loss','private_property_value_loss','total_cost'))
